a0e35178ea0d01981dc6d799c923585316c22bdc
Dinesh kumar <www.dineshk40@gmail.com>
2023-12-20 08:15:51 +0000

    Added GFG POTD December 20, 2023

---
+++ java_codes/2023/december/20-Modified Game Of Nim.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(read.readLine());
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int A[]= new int[n];
+            for(int i = 0; i < n; i++)
+                A[i] = Integer.parseInt(input_line[i]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.findWinner(n, A));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+//User function Template for Java
+class Solution{
+    static int findWinner(int n, int A[]){
+        int xor = 0;
+        
+        for(int i: A)
+            xor ^= i;
+        
+        //Edge Case: if xor of all element is already 0, then player 1 wins
+        if(xor == 0)
+            return 1;
+        else
+            return (n%2) + 1;
+    }
+}
